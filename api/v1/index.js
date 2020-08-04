50ee25cd7bd6e3f12e45d0a1faade2f071a61ab7
Anis AJMI <anis.a@soloinvest.com>
2020-08-04 17:55:52 +0200

    Tester la connection vers mongoDB

--- api/v1/index.js
+++ api/v1/index.js
@@ -12,8 +12,7 @@ router.get('/blog-posts', (req, res) => {
 			title: 'Etre programmeur ça pique parfois !!!',
 			subTitle: 'ce cactus est pas là pour rien',
 			image: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg',
-			content:
-        'I\'m a thing. But, like most politicians, he promised more than he could deliver. You won\'t have time for sleeping, soldier. not with all the bed making you\'ll be doing. Then we\'ll go with that data file! Hey, you add a one and two zeros to that or we walk! You\'re going to do his laundry? I\'ve got to find a way to escape.',
+			content: 'I\'m a thing. But, like most politicians, he promised more than he could deliver. You won\'t have time for sleeping, soldier. not with all the bed making you\'ll be doing. Then we\'ll go with that data file! Hey, you add a one and two zeros to that or we walk! You\'re going to do his laundry? I\'ve got to find a way to escape.',
 		},
 		{
 			id: 2,
--- app.js
+++ app.js
@@ -2,18 +2,30 @@ const express = require('express');
 const app = express();
 const api = require('./api/v1/index');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const { Connection } = require('mongoose');
+const connection = mongoose.connection;
 
 app.set('port', (process.env.port || 3000));
 
 app.use(cors());
 app.use('/api/v1', api);
 app.use((req, res) => {
-    const err = new Error('404 - Not founf !!!');
+    const err = new Error('404 - Not found !!!');
     err.Status = 500;
-    res.json({msg: '404 - Not founf !!!', err: err})
+    res.json({msg: '404 - Not found !!!', err: err})
 });
 
-app.listen(app.get('port'), () => {
-    console.log(`Express serve listening on port ${app.get('port')}`);
+mongoose.connect('mongodb://localhost:27017/whiskycms', {useNewUrlParser: true, useUnifiedTopology: true});
+connection.on('error', (err) => {
+    console.error(`Connection to MongoDB error : ${err.msg}`);
+});
+
+connection.once('open', () => {
+    console.log('Connected to MongoDB');
+
+    app.listen(app.get('port'), () => {
+        console.log(`Express serve listening on port ${app.get('port')}`);
+    });
 });
 
