a27f4cbea674cb0165db4977516a697db181a39a
Anis AJMI <anis.a@soloinvest.com>
2020-08-07 10:52:20 +0200

    DELETE BY ID in MongoDB

--- api/v1/index.js
+++ api/v1/index.js
@@ -15,7 +15,7 @@ router.get('/blog-posts', (req, res) => {
         message: 'blog posts not found - :(',
         error: err
     }));
-}); //http://localhost:3000/api/v1/blog-posts
+}); // FIND ALL : http://localhost:3000/api/v1/blog-posts
 
 router.post('/blog-posts', (req, res) => {
 	console.log('req.body', req.body);
@@ -26,6 +26,26 @@ router.post('/blog-posts', (req, res) => {
 		}
 		res.status(201).json(blogPost);
 	});
-});
+}); // POST
+
+router.get('/blog-posts/:id', (req, res) => {
+    const id = req.params.id;
+    Blogpost.findById(id)
+        .then(blogPost => res.status(200).json(blogPost))
+        .catch(err => res.status(500).json({
+            message: `blog post whith id ${id} not found`,
+            error: err
+        }));
+}); // Find BY ID
+
+router.delete('/blog-posts/:id', (req, res) => {
+    const id = req.params.id;
+    Blogpost.findByIdAndDelete(id, (err, blogPost) => {
+        if(err) {
+            return res.status(500).json(err);
+        }
+        res.status(200).json({ meg: `blog post with id ${blogPost.id} deleted`});
+    });
+}); //DELETE
 
 module.exports = router;
