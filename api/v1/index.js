d92714a06ecd0bf5743fe124141f331e6cb9a40b
Anis AJMI <anis.a@soloinvest.com>
2020-08-24 17:36:26 +0200

    Upload image

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Blogpost = require('../models/blogpost');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const crypto = require('crypto');
+const path = require('path');
 
 router.get('/ping', (req, res) => {
     res.status(200).json({ msg: 'pong', date: new Date(), methode : req.method});
@@ -17,9 +21,35 @@ router.get('/blog-posts', (req, res) => {
     }));
 }); // FIND ALL : http://localhost:3000/api/v1/blog-posts
 
+// file upload configuration
+const storage = multer.diskStorage({
+	destination: './uploads/',
+	filename: function (req, file, callback) {
+		crypto.pseudoRandomBytes(16, function(err, raw) {
+			if (err) return callback(err);
+			//callback(null, raw.toString('hex') + path.extname(file.originalname));
+			lastUploadedImageName = raw.toString('hex') + path.extname(file.originalname);
+			console.log('lastUploadedImageName', lastUploadedImageName);
+			callback(null, lastUploadedImageName);
+		});
+	}
+});
+
+const upload = multer({storage: storage});
+
+// file upload route
+router.post('/blog-posts/images', upload.single('image'), (req, res) => {
+	// console.log('req.file', req.file);
+	if (!req.file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+		return res.status(400).json({ msg: 'only image files please'});
+	}
+	res.status(201).send({ fileName: req.file.filename, file: req.file });
+});
+
 router.post('/blog-posts', (req, res) => {
 	console.log('req.body', req.body);
-	const blogPost = new Blogpost(req.body);
+	// const blogPost = new Blogpost(req.body);
+	const blogPost = new Blogpost({...req.body, image: lastUploadedImageName});
 	blogPost.save((err, blogPost) => {
 		if (err) {
 			return res.status(500).json(err);
@@ -27,6 +57,8 @@ router.post('/blog-posts', (req, res) => {
 		res.status(201).json(blogPost);
 	});
 }); // POST
+
+let lastUploadedImageName = '';
 
 router.get('/blog-posts/:id', (req, res) => {
     const id = req.params.id;
@@ -39,13 +71,36 @@ router.get('/blog-posts/:id', (req, res) => {
 }); // Find BY ID
 
 router.delete('/blog-posts/:id', (req, res) => {
-    const id = req.params.id;
-    Blogpost.findByIdAndDelete(id, (err, blogPost) => {
-        if(err) {
-            return res.status(500).json(err);
-        }
-        res.status(200).json({ meg: `blog post with id ${blogPost.id} deleted`});
-    });
-}); //DELETE
+	const id = req.params.id;
+	console.log('delete by id', id);
+	Blogpost.findByIdAndDelete(id, (err, blogPost) => {
+		if (err) {
+			return res.status(500).json(err);
+		}
+		res.status(202).json({ msg: `blog post with id ${id} deleted`});
+	});
+});
+
+router.delete('/blog-posts', (req, res) => {
+	// retrieves the query parameter: http://localhost:3000/api/v1/blog-posts?ids=5c1133b8225e420884687048,5c1133b6225e420884687047
+	const ids = req.query.ids;
+	console.log('query allIds', ids);
+	const allIds = ids.split(',').map(id => {
+		// casting as a mongoose ObjectId	
+		if (id.match(/^[0-9a-fA-F]{24}$/)) {
+			return mongoose.Types.ObjectId((id));		 
+		}else {
+			console.log('id is not valid', id);
+			return -1;
+		}
+	});
+	const condition = { _id: { $in: allIds} };
+	Blogpost.deleteMany(condition, (err, result) => {
+		if (err) {
+			return res.status(500).json(err);
+		}
+		res.status(202).json(result);
+	});
+});
 
 module.exports = router;
